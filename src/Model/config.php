7a0b2ab9b101927e7f52664647d12f10bdcddd97
teleyos <tristanduret@hotmail.fr>
2021-04-08 09:10:32 +0200

    auto commit

--- src/Model/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("hostname","serveurmysql"); // localhost
-define("database","S2_bdd");
-define("username","votreLogin");
-define("password","votreMotDePasse");
-
-
-// php bin/console debug:container --env-vars
